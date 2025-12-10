da46222e1629f5a5f77bae31c14e1beb24cc34e8
Antonio Guagliardi <antonio.guagliardi@rse-web.it>
2025-12-10 15:30:37 +0100

    corretta la installazione di gdbm-install

--- gdbm-install/install.sh
+++ gdbm-install/install.sh
@@ -2,7 +2,8 @@
 #   Procedura di installazione di libgdbm.so.2 da pacchetto rpm (vedere readme.txt)
 # 1) Download dl paccketto rpm: curl -O "ftp://ftp.icm.edu.pl/vol/rzm6/pbone/archive.fedoraproject.org/fedora/linux/releases/13/Everything/x86_64/os/Packages/gdbm-1.8.0-33.fc12.x86_64.rpm"
 # 2) Estrazione della libreria: 
-rpm2cpio gdbm-1.8.0-33.fc12.x86_64.rpm | cpio -idmv
+(cd $LEGOROOT/gdbm-install && rpm2cpio gdbm-1.8.0-33.fc12.x86_64.rpm | cpio -idmv)
+#rpm2cpio gdbm-1.8.0-33.fc12.x86_64.rpm | cpio -idmv
 # 3) Installazione ...
 sudo cp $LEGOROOT/gdbm-install/usr/lib64/libgdbm.so.2.0.0  /usr/lib64/
 sudo ln -s /usr/lib64/libgdbm.so.2.0.0 /usr/lib64/libgdbm.so.2
